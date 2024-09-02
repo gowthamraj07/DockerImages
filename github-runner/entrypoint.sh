8ca1f9c899a6d9e553a2439b15a48ac56a47610d
Gowtham Shanmugaraj <gowthamraj07@gmail.com>
2024-09-02 15:05:57 +0200

    Update entrypoint.sh to use custom labels and docker name as runner name

--- github-runner/entrypoint.sh
+++ github-runner/entrypoint.sh
@@ -17,12 +17,15 @@ if [ -z "$REPO_URL" ] || [ -z "$TOKEN" ]; then
     exit 1
 fi
 
+# Determine the runner name
+RUNNER_NAME=${DOCKER_NAME:-$(hostname)}
+
 # Check if the runner is already configured
 if [ -f ".runner" ]; then
     echo "Runner already configured. Skipping registration."
 else
     echo "Configuring runner..."
-    ./config.sh --url $REPO_URL --token $TOKEN --name $(hostname) --work _work --replace --unattended
+    ./config.sh --url $REPO_URL --token $TOKEN --name $RUNNER_NAME --work _work --replace --unattended --labels $LABELS
 fi
 
 # Start the runner
